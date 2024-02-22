af8a5e0d3d528c0e6fc7098924232c339a0c59c7
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-02-22 18:11:56 +0000

    Add comments to test script

--- test.py
+++ test.py
@@ -9,9 +9,11 @@
 import requests
 import imgmin
 
+# Download example image
 url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/SMPTE_Color_Bars.svg/2560px-SMPTE_Color_Bars.svg.png"
 
 r = requests.get(url, allow_redirects=True)
 open("test.png", "wb").write(r.content)
 
+# Minify the image
 imgmin.generate_minified_images("test.png")
